b28bb81b0571b350bb1483ec882442616e2cf19e
artisan.tarun <artisan.tarun@gmail.com>
2020-05-07 16:32:56 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Algorithms</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/script.js"></script>
+  </head>
+  <body>
+      <?php echo "Hello World!!!!!!!!"; ?>
+  </body>
+</html>
