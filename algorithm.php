0e5a737e650417c797ed5ee8eedc25b63b7bbb96
thakursaab0009 <60252067+thakursaab0009@users.noreply.github.com>
2020-05-18 13:43:32 +0530

    algorithm

--- algorithm.php
+++ algorithm.php
@@ -1,13 +1,44 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <?php include('includes/head.php') ?>
-  </head>
-  <body>
-      <?php include('includes/navigation.php') ?>
-    <div class="container-fluid">
-        make algorithms app here code everything inside this page we'll create team page leter
-        after making changes push to github using github desktop app
+<DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8" />
+    <title>First Project</title>
+    <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    <div>
+    <div id="body">
+        
+    
+    
+        <section id="main_section">
+            <article>
+                <button id="btn">Start</button>
+                       
+                      
+              </article>
+        
+            <article>
+              <h1>Algorithm 1 </h1>
+              <p>This is the first part of the algorithm</p>
+              
+                     
+                    
+            </article>
+    
+            <article>
+                <h1>Algorithm 2</h1>
+                <p>This the second part of algorithm</p>
+                   
+                 
+           
+         </article>
+        </section>
+    
     </div>
-  </body>
-</html>
+    <footer id="the_footer">
+        AppBuddha.tech 2020
+    </footer>
+    </div>
+    </body>
+    </html>
